14788786cdb47020b279e2687e1137b37bfcf6c0
Mark Hsu <mark.g.hsu@gmail.com>
2024-01-19 19:26:16 -0800

    Add permission

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ navigator.geolocation.getCurrentPosition(position => {
             const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
             document.getElementById("weather").innerHTML = `
                 <img class="weather-icon" src=${iconUrl} />
-                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                <p class="weather-temp">${Math.round(data.main.temp)}°</p>
                 <p class="weather-city">${data.name}</p>
             `
         })
